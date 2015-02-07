b1d3352d0c1f4a28aeb964a225bd2bcacf6e799f
Ork <ork@olol.eu>
2015-02-07 14:12:08 +0100

    Flag register offset

--- src/registers.h
+++ src/registers.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 struct _registers {
     uint8_t a; // Accumulator
     uint8_t f; // Flags
@@ -14,4 +16,20 @@ struct _registers {
     uint16_t sp; // Stage pointer
 };
 typedef struct _registers registers_t;
+
+/* Bits in the flag register
+ * ┌─┬─┬─┬─┬─┬─┬─┬─┐
+ * │7│6│5│4│3│2│1│0│
+ * ├─┼─┼─┼─┼─┼─┼─┼─┤
+ * │Z│N│H│C│0│0│0│0│
+ * └─┴─┴─┴─┴─┴─┴─┴─┘
+ */
+
+enum _flag_offset {
+    C = 0b00010000, // Carry
+    H = 0b00100000, // Half Carry
+    N = 0b01000000, // Subtract
+    Z = 0b10000000, // Zero
+};
+typedef enum _flag_offset flag_offset_t;
 

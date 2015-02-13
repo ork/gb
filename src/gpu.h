e5f76e78925fc90c3c34fcba6e73a7b0436da03f
Ork <ork@olol.eu>
2015-02-13 20:16:38 +0100

    Add the LCD Control Register structures

--- src/gpu.h
+++ src/gpu.h
@@ -9,17 +9,16 @@
 
 struct _gpu {
     uint8_t RAM[VRAM_SIZE]; // 8kB Video RAM
-    uint8_t OAM[OAM_SIZE]; // 160B Sprite Attribute Table
+    uint8_t OAM[OAM_SIZE];  // 160B Sprite Attribute Table
 
-    uint8_t LCDC;
-    uint8_t SCX;
-    uint8_t SCY;
-    uint8_t LY;
-    uint8_t LYC;
-    uint8_t BGP;
-    uint8_t OBP0;
-    uint8_t OBP1;
-    uint8_t WX;
-    uint8_t WY;
+    // LCD Control
+    bool lcd_on;            // LCD Display Enable
+    uint16_t win_tilemap;   // Window Tile Map Display Select
+    bool win_on;            // Window Display Enable
+    uint16_t tile_data;     // BG & Window Tile Data Select
+    uint16_t bg_tilemap;    // BG Tile Map Display Select
+    size_t sprite_size;     // OBJ (Sprite) Size
+    bool sprite_on;         // OBJ (Sprite) Display Enable
+    bool lcdc0;             // LCDC0 (see pandocs)
 };
 typedef struct _gpu gpu_t;
